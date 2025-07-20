576ad8d13af6befe8a21884414a37183b3dfe509
Gustavo <maiagustavo125@gmail.com>
2025-07-20 16:54:58 -0300

    feat: footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <footer className='site-footer'>
+            <p>&copy; 2025 Gustavo. Todos os direitos reservados.</p>
+        </footer>
+    );
+}
